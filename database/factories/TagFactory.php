ad0db783ffe3f575b82bb3d67252bfd0f2b1f585
Abdelrahman-Labib <35099977+Abdelrahman-Labib@users.noreply.github.com>
2022-06-06 03:14:06 +0200

    Tag Factory

---
+++ database/factories/TagFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Advertisement;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tag>
+ */
+class TagFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'advertisement_id'  => Advertisement::factory()->create()->id,
+            'name'              => $this->faker->title
+        ];
+    }
+}
