6e288fdee44aeab815f78bccc1613409953a875e
Abdelrahman-Labib <35099977+Abdelrahman-Labib@users.noreply.github.com>
2022-06-06 02:26:40 +0200

    Helper Constant

---
+++ app/Helpers/Constant.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Helpers;
+
+class Constant
+{
+    const ADVERTISEMENT_TYPE = [
+        'Free'  => 1,
+        'Paid'  => 2,
+    ];
+
+    const CATEGORY_STATUS = [
+        'Active'     => 1,
+        'Not active' => 2
+    ];
+}
