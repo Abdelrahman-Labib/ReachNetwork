ae4b40c8016b5855468c4f118a3ba047203e9b1b
Abdelrahman-Labib <35099977+Abdelrahman-Labib@users.noreply.github.com>
2022-06-06 02:15:38 +0200

    Api Route

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\Api\V100\CategoryController;
+use App\Http\Controllers\Api\V100\TagController;
+use App\Http\Controllers\Api\V100\AdvertiserController;
+use App\Http\Controllers\Api\V100\AdvertisementController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('/v1')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+
+    Route::apiResource('tags', TagController::class);
+
+    Route::get('advertisement/filter', AdvertisementController::class);
+
+    Route::get('advertiser/{user}', AdvertiserController::class);
 });
