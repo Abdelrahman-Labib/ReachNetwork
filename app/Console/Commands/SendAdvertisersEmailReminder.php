1ef0e0663264505b09c117e1058f2569b947649f
Abdelrahman-Labib <35099977+Abdelrahman-Labib@users.noreply.github.com>
2022-06-06 02:29:59 +0200

    schedule daily mail at specific time  with job

---
+++ app/Console/Commands/SendAdvertisersEmailReminder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\SendAdvertiserReminderEmail;
+use App\Repository\AdvertiserRepositoryInterface;
+use Illuminate\Console\Command;
+
+class SendAdvertisersEmailReminder extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'advertisers:reminder';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'schedule a daily email at 08:00 PM that will be sent to advertisers who have ads the next day as a remainder.';
+
+    protected $advertiserRepository;
+
+    public function __construct(AdvertiserRepositoryInterface $advertiserRepository)
+    {
+        $this->advertiserRepository = $advertiserRepository;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        dispatch(new SendAdvertiserReminderEmail($this->advertiserRepository));
+    }
+}
---
+++ resources/views/emails/advertiser-mails.blade.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<body>
+<p>Dear {{ $name }},</p>
+<p>This is a reminder email for your advertisements tomorrow</p>
+<p>Best of Luck</p>
+</body>
