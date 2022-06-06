45d695999b9a3afe1b14267ddde7902804610187
Abdelrahman-Labib <35099977+Abdelrahman-Labib@users.noreply.github.com>
2022-06-06 02:24:17 +0200

    EloquentRepositoryInterface & BaseRepository

---
+++ app/Repository/Eloquent/BaseRepository.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Repository\Eloquent;
+
+use App\Repository\EloquentRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+class BaseRepository implements EloquentRepositoryInterface
+{
+    /**
+     * @var Model
+     */
+    protected $model;
+
+    /**
+     * BaseRepository constructor.
+     *
+     * @param Model $model
+     */
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * @param array $columns
+     * @param array $relations
+     * @return Collection
+     */
+    public function index(array $columns = ['*'], array $relations = []): Collection
+    {
+        return $this->model->with($relations)->get($columns);
+    }
+
+    /**
+     * Find model by id.
+     *
+     * @param int $modelId
+     * @param array $columns
+     * @param array $relations
+     * @param array $appends
+     * @return Model
+     */
+    public function show(
+        int $modelId,
+        array $columns = ['*'],
+        array $relations = [],
+        array $appends = []
+    ): ?Model {
+        return $this->model->select($columns)->with($relations)->findOrFail($modelId)->append($appends);
+    }
+
+    /**
+     * Create a model.
+     *
+     * @param array $payload
+     * @return Model
+     */
+    public function create(array $payload): ?Model
+    {
+        $model = $this->model->create($payload);
+
+        return $model->fresh();
+    }
+
+    /**
+     * Update existing model.
+     *
+     * @param int $modelId
+     * @param array $payload
+     * @return bool
+     */
+    public function update(int $modelId, array $payload): bool
+    {
+        $model = $this->model->find($modelId);
+
+        return $model->update($payload);
+    }
+
+    /**
+     * Delete model by id.
+     *
+     * @param int $modelId
+     * @return bool
+     */
+    public function delete(int $modelId): bool
+    {
+        return $this->model->find($modelId)->delete();
+    }
+}
